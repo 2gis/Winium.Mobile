c3e9044b38a4e3199164c7756754f82ea1a8d994
Golovin Gleb <g.golovin@2gis.ru>
2015-02-17 18:01:39 +0600

    Add test application with two simple elements

---
+++ WindowsUniversalAppDriver/TestApp/TestApp.Windows/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TestApp
+{
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
