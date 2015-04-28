b54820dd748300ffb2ba9ad8843d4581ea6c6786
Nick Abalov <n.abalov@2Gis.ru>
2015-04-28 14:48:40 +0600

    Move touch screen initialization to property getter
    Init TouchScreen when needed instead of initing it in each constructor

--- Winium/TestApp.Test/Samples/WpDriver.cs
+++ Winium/TestApp.Test/Samples/WpDriver.cs
@@ -1,36 +1,56 @@
 ﻿namespace Samples
 {
+    #region
+
     using System;
 
     using OpenQA.Selenium;
     using OpenQA.Selenium.Remote;
 
+    #endregion
+
     public class WpDriver : RemoteWebDriver, IHasTouchScreen
     {
+        #region Fields
+
+        private ITouchScreen touchScreen;
+
+        #endregion
+
+        #region Constructors and Destructors
+
         public WpDriver(ICommandExecutor commandExecutor, ICapabilities desiredCapabilities)
             : base(commandExecutor, desiredCapabilities)
         {
-            TouchScreen = new RemoteTouchScreen(this);
         }
 
         public WpDriver(ICapabilities desiredCapabilities)
             : base(desiredCapabilities)
         {
-            TouchScreen = new RemoteTouchScreen(this);
         }
 
         public WpDriver(Uri remoteAddress, ICapabilities desiredCapabilities)
             : base(remoteAddress, desiredCapabilities)
         {
-            TouchScreen = new RemoteTouchScreen(this);
         }
 
         public WpDriver(Uri remoteAddress, ICapabilities desiredCapabilities, TimeSpan commandTimeout)
             : base(remoteAddress, desiredCapabilities, commandTimeout)
         {
-            TouchScreen = new RemoteTouchScreen(this);
         }
 
-        public ITouchScreen TouchScreen { get; private set; }
+        #endregion
+
+        #region Public Properties
+
+        public ITouchScreen TouchScreen
+        {
+            get
+            {
+                return this.touchScreen ?? (this.touchScreen = new RemoteTouchScreen(this));
+            }
+        }
+
+        #endregion
     }
 }
