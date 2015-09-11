7be57cf7fd7ea76b4ea61543919cedd85e3fe0ee
Magnar Nedland <magnar.nedland@telenordigital.com>
2015-09-11 14:26:48 +0200

    Use ToString on exceptions to get stack trace of inner exceptions too.

--- Winium/Winium.StoreApps.InnerServer/Commands/CommandBase.cs
+++ Winium/Winium.StoreApps.InnerServer/Commands/CommandBase.cs
@@ -43,11 +43,11 @@
             }
             catch (AutomationException exception)
             {
-                response = this.JsonResponse(exception.Status, exception);
+                response = this.JsonResponse(exception.Status, exception.ToString());
             }
             catch (Exception exception)
             {
-                response = this.JsonResponse(ResponseStatus.UnknownError, exception);
+                response = this.JsonResponse(ResponseStatus.UnknownError, exception.ToString());
             }
 
             return response;
