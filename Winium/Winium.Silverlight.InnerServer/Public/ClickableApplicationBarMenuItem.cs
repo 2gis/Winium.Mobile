d2cacb670039db7853a1338914860881189ac22f
Alexander Bayandin <a.bayandin@gmail.com>
2016-10-10 16:30:42 +0100

    Move InnerServer from winphonedriver

---
+++ Winium/Winium.Silverlight.InnerServer/Public/ClickableApplicationBarMenuItem.cs
@@ -0,0 +1,28 @@
+﻿namespace Winium.Silverlight.InnerDriver.Public
+{
+    using System;
+    using Microsoft.Phone.Shell;
+
+    public class ClickableApplicationBarMenuItem : ApplicationBarMenuItem
+    {
+        public ClickableApplicationBarMenuItem()
+            : base()
+        {
+        }
+
+        public ClickableApplicationBarMenuItem(string text)
+            : base(text)
+        {
+        }
+
+        public new event EventHandler Click;
+
+        public void RaiseClick()
+        {
+            if (this.Click != null)
+            {
+                this.Click(this, EventArgs.Empty);
+            }
+        }
+    }
+}
