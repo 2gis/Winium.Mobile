b92c8145ac8073d2fd0c91446f8011ca2265708a
Alexander Bayandin <a.bayandin@gmail.com>
2016-10-13 20:59:23 +0100

    Fix app path for functional tests for silverlight

--- Winium/TestApp.Test/py-functional/config_silverlight.py
+++ Winium/TestApp.Test/py-functional/config_silverlight.py
@@ -1,10 +1,9 @@
 # coding: utf-8
 import os
 
-BUILD_CONFIG = os.environ.get('BUILD_CONFIG', 'Debug')
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-AUT_PATH = r"..\TestApp.Silverlight\Bin\x86\{0}\TestApp_{0}_x86.xap".format(BUILD_CONFIG)
+CONFIG_IDENTIFIER = 'Release' if os.getenv('REMOTE_RUN') else 'Debug'
+AUT_PATH = r"..\TestApp.Silverlight\Bin\{0}\TestApp_{0}_AnyCPU.xap".format(CONFIG_IDENTIFIER)
 
 DESIRED_CAPABILITIES = {
     "deviceName": "Emulator 8.1",
